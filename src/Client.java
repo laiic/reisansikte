2dcf0dadec663af91ce9893bfe7fe1c2feb4493e
MasterReji <reimondo@kth.se>
2017-01-06 22:43:07 +0100

    TEHTA

--- src/Client.java
+++ src/Client.java
@@ -170,11 +170,11 @@ public class Client implements Runnable {
             }
 
             else if(msg.equals("OK")) {
-//                try {
-//                    queue.put("OK");
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
+                try {
+                    queue.put("OK");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
                 try {
                     sipLogic.processNextEvent(SIPEvent.SEND_OK);
